136c5782512a5e49a82151854aebfd133c40998e
Kelsey Langlois and Luciano Oliveira <kelsey@>
2017-09-07 11:01:03 -0700

    Add function to properly format directory name and remove bug in tests.

--- lib/project_creator.rb
+++ lib/project_creator.rb
@@ -2,9 +2,13 @@ require 'fileutils'
 
 class ProjectCreator
 
+  def make_directory_name(name)
+    name.downcase.split.join("_")
+  end
+
   def make_project(name)
     begin
-      FileUtils.mkdir("../" + name)
+      FileUtils.mkdir("../" + make_directory_name(name))
     rescue SystemCallError => e
       return e.class
     end
--- spec/project_creator_spec.rb
+++ spec/project_creator_spec.rb
@@ -6,8 +6,9 @@ describe("ProjectCreator#make_project") do
   it("creates root directory with inputted name") do
     creator = ProjectCreator.new()
     creator.make_project("new_project")
-    FileUtils.cd("../new_project")
-    expect(FileUtils.pwd()).to(eq("/Users/Guest/Desktop/new_project"))
+    FileUtils.cd("../new_project") {
+      expect(FileUtils.pwd()).to(eq("/Users/Guest/Desktop/new_project"))
+    }
   end
 
   it("will not create new directory with name if directory already exists") do
@@ -15,5 +16,12 @@ describe("ProjectCreator#make_project") do
     error = creator.make_project("new_project")
     expect(error).to(eq(Errno::EEXIST))
     FileUtils.remove_dir("../new_project")
+  end
+end
+
+describe("ProjectCreator#make_directory_name") do
+  it("properly formats multi-word directory name") do
+    creator = ProjectCreator.new()
+    expect(creator.make_directory_name("New Project")).to(eq("new_project"))
   end
 end
